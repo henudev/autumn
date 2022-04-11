96bfc2df7a511a28c22f424038160e56e01a1a72
qinzheng <qinzheng@h3c.com>
2022-04-11 15:00:54 +0800

    增加redis集成和logbak日志

--- mid-redis/src/main/java/redis/config/JedisRedisConfig.java
+++ mid-redis/src/main/java/redis/config/JedisRedisConfig.java
@@ -1,10 +1,18 @@
-package config;
+package redis.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+
 @Configuration
 @ConditionalOnProperty(value="spring.redis.jedis.enable",havingValue="true", matchIfMissing=false)
+@Slf4j
 public class JedisRedisConfig {
-
+    @PostConstruct
+    public void test(){
+        log.warn("jedis DONE!!!!");
+    }
 }
---
+++ mid-redis/src/main/test/java/SpringBootRedisApplicationTests.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootRedisApplicationTests {
+    @Test
+    public void test(){
+
+    }
+}
